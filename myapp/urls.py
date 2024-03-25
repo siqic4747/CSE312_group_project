a63e479f7a57fd2acc939dc2130cbf6eb01f9a8d
siqic4747 <scheng42@buffalo.edu>
2024-03-24 22:23:53 -0400

    add register

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
 from django.urls import include
 from . import views
+from .views import register
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('register/', views.register, name='register'),
     path('forgot-password-request/', views.forgot_password_request, name='forgot_password_request'),
     path('register-form/', views.register_form, name= 'register_form'),
+    path('login/', LoginView.as_view(template_name='index.html'), name='login'),
+    path('logout/', LogoutView.as_view(next_page='index'), name='logout'),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,17 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .forms import RegisterForm
+
 
 def index(request):
     # Your index view logic here
     return render(request, 'index.html')
 
 def register(request):
-    # Your register view logic here
-    return render(request, 'register.html')
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            return redirect('index') 
+    else:
+        form = RegisterForm()
+    return render(request, 'register.html', {'form': form})
 
 def register_form(request):
-    #get the data from the POST request and redirect the user to the login page
-    return
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            return redirect('index') 
+        else:
+            return render(request, 'register.html', {'form': form}) 
+    else:
+        form = RegisterForm()
+
+    return render(request, 'register.html', {'form': form})  
 
 def forgot_password_request(request):
     # Your forgot password request view logic here
