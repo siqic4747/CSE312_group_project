b886a7961ccbb1b7b425c4b7835a8914c72dfaa8
siqic4747 <scheng42@buffalo.edu>
2024-03-24 23:05:05 -0400

    add login

--- myapp/forms.py
+++ myapp/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import AuthenticationForm
+
 
 User = get_user_model()
 
@@ -17,3 +19,6 @@ class RegisterForm(UserCreationForm):
         if commit:
             user.save()
         return user
+class CustomLoginForm(AuthenticationForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'input-box', 'placeholder': 'Username'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'input-box', 'placeholder': 'Password'}))
